cbe1df34c9db18c1c953f3bea340c30faaf7ec33
barsia <barsia@users.noreply.github.com>
2025-11-17 22:23:05 +0100

    Add sample project for Android, iOS, and Desktop with Gradle and Compose setup

---
+++ src/test/resources/fixtures/android-ios-desktop-all-libraries-tests-1.10-beta01/composeApp/src/commonTest/kotlin/org/example/project/android/desktop/ios/tests/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package org.example.project.android.desktop.ios.tests
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ src/test/resources/fixtures/android-ios-desktop-all-libraries-tests-1.10-beta01/settings.gradle.kts
@@ -0,0 +1,27 @@
+rootProject.name = "AndroidDesktopIosAllLibraries"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+include(":composeApp")
